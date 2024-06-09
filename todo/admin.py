832b065a655f8e5e36703d328f405b948b384c21
Amit Jaiswal <amitjaisawal0123@gmail.com>
2024-06-09 12:35:23 +0530

    created admin page

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Task, Learning
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'due_date', 'completed')
+    search_fields = ('title', 'user__username')
+    list_filter = ('completed', 'due_date', 'user')
+
+@admin.register(Learning)
+class LearningAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'content')
+    search_fields = ('user__username', 'content')
+    list_filter = ('date', 'user')
